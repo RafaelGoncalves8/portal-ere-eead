2c10b448c528e9cb9d4079cf18e7b02e77ddb242
Rafael Goncalves <rafaelgoncalves@riseup.net>
2020-09-06 00:13:27 -0300

    Test deploy

---
+++ docs/js/chunk-2d0d0435.58bfb3a6.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0d0435"],{"66dc":function(n,u,c){"use strict";c.r(u);var l,d,e=c("2877"),o={},p=Object(e["a"])(o,l,d,!1,null,null,null);u["default"]=p.exports}}]);
+//# sourceMappingURL=chunk-2d0d0435.58bfb3a6.js.map
---
+++ docs/js/chunk-2d212b1c.8f8a8cf4.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d212b1c"],{aa06:function(n,u,a){"use strict";a.r(u);var c,l,e=a("2877"),o={},p=Object(e["a"])(o,c,l,!1,null,null,null);u["default"]=p.exports}}]);
+//# sourceMappingURL=chunk-2d212b1c.8f8a8cf4.js.map
